8d474309fe33a9672a9eadd0390eb01bec978eb3
root <mail2yousuff@gmail.com>
2018-08-05 08:41:49 +0000

    Changes made in return statement

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 def hello_world():
 
-    return '<h2>Hey, we have Flask in a Docker container!</h2>'
+    return '<h3>Hey, we have Flask in a Docker container!</h3>'
     
 
 if __name__ == '__main__':
